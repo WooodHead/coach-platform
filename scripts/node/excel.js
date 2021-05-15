6bfb67f3b64756697b5d337a4372688bdf5d88b2
Tiim <tim.bachmann96@gmail.com>
2021-05-15 15:35:13 +0200

    feat: improve the xlsx to sql importer

--- scripts/node/excel.js
+++ scripts/node/excel.js
@@ -1,22 +1,32 @@
 import xlsx from 'node-xlsx';
 import { v5 } from 'uuid';
 import pgPromise from 'pg-promise';
-
+import fsExtra from 'fs-extra';
+import { ArgumentParser } from 'argparse';
 
 // CONFIG:
-const organisation_id = '0ddb3d79-a59b-48af-8829-b264e30a9001'
 const uuidNamespace = 'c4556bf7-03ff-4a99-8969-2226b06f0d47'
 // END CONFIG
 
 const uuid = (name) => v5(name, uuidNamespace);
 const pgp = pgPromise();
-const files = process.argv.slice(2);
 
 
-console.warn("Files", files);
+const parse = new ArgumentParser();
+
+parse.add_argument('-m', '--name-map', {type: "str"});
+parse.add_argument('files', {"type": "str", "nargs":"+"})
+parse.add_argument('-d', '--debug', {type: "str", choices: ["student"]});
+parse.add_argument('--org', {type: "str", required: true})
+const args = parse.parse_args(process.argv.slice(2));
+
+const organisation_id = args.org
+let nameMap = args.name_map ? await fsExtra.readJson(args.name_map) : {}
+
+console.warn("Files", args.files);
 
 console.warn('Reading excel')
-const worksheets = files.map(file => xlsx.parse(file));
+const worksheets = args.files.map(file => xlsx.parse(file));
 
 console.warn('Crunching data')
 const entries = worksheets.flatMap(worksheet => worksheet.flatMap((value) => handleSheet(value.name, value.data))
@@ -44,18 +54,23 @@ console.warn("Lessons:", lessons.length)
 
 
 const attendance = entries.map(e => ({student_id: studentsIds[e.name], lesson_id: lessonsIds[e.day.toISOString()], state: e.state}))
+console.warn("Attendance Entries:", attendance.length)
 
 lessons.sort()
 //students.sort()
 
-const sql_students = pgp.helpers.insert(students, ['id', 'name', 'organisation_id'], 'student') + ' ON CONFLICT DO NOTHING';
-const sql_lessons = pgp.helpers.insert(lessons, ['id', 'start_time', 'name', 'organisation_id'], 'lesson') + ' ON CONFLICT DO NOTHING';
-const sql_attendance = pgp.helpers.insert(attendance, ['student_id', 'lesson_id', 'state'], 'student_attendance') + ' ON CONFLICT DO NOTHING';
 
-const sql_all = pgp.helpers.concat([sql_students, sql_lessons, sql_attendance]);
+if (!args.debug) {
+  const sql_students = pgp.helpers.insert(students, ['id', 'name', 'organisation_id'], 'student') + ' ON CONFLICT DO NOTHING';
+  const sql_lessons = pgp.helpers.insert(lessons, ['id', 'start_time', 'name', 'organisation_id'], 'lesson') + ' ON CONFLICT DO NOTHING';
+  const sql_attendance = pgp.helpers.insert(attendance, ['student_id', 'lesson_id', 'state'], 'student_attendance') + ' ON CONFLICT DO NOTHING';
 
-console.log(sql_all)
+  const sql_all = pgp.helpers.concat([sql_students, sql_lessons, sql_attendance]);
 
+  console.log(sql_all)
+} else if (args.debug === 'student') {
+  console.log(JSON.stringify(students.map(s=> s.name).sort(), null, 2));
+}
 
 
 /**
@@ -120,7 +135,8 @@ function mapState(state) {
 }
 
 function mapName(name) {
-  return name
+  const n = name
   .replace(/\(.*\)?/g, '')
   .trim()
+  return nameMap[n] ?? n;
 }
